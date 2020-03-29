86250f4e5b4d3f247989d5cc20922802ff49dbda
Kirsi84 <kirsi.rouvinen@edu.omnia.fi>
2020-03-29 23:09:30 +0300

    Added database data

--- Users/db_connect() Function from db_fns.php
+++ Users/db_connect() Function from db_fns.php
@@ -1,8 +1,17 @@
 <?php
 
 function db_connect() {
-   $result = new mysqli('localhost', 'bm_user', 'password', 'bookmarks');
-   if (!$result) {
+    // KIR 20200329
+    $local = ($_SERVER['REMOTE_ADDR']=='127.0.0.1' || $_SERVER['REMOTE_ADDR']=='::1');
+    if (!$local )
+    {
+        $result = new mysqli('127.0.0.1:53181', 'azure', '6#vWHD_$', 'sports');
+    }
+    else {
+        //$result = new mysqli('localhost', 'bm_user', 'password', 'bookmarks');
+        $result = new mysqli('localhost', 'root', '', 'sports');
+    }
+     if (!$result) {
      throw new Exception('Could not connect to database server');
    } else {
      return $result;
--- Users/db_fns.php
+++ Users/db_fns.php
@@ -3,7 +3,21 @@
 function db_connect() {
   // $result = new mysqli('localhost', 'bm_user', 'password', 'bookmarks');
   //$result = new mysqli('localhost', 'root', '', 'bookmarks');
-  $result = new mysqli('localhost', 'root', '', 'sports');
+ // $result = new mysqli('localhost', 'root', '', 'sports');
+
+	
+		 // KIR 20200329
+     $local = ($_SERVER['REMOTE_ADDR']=='127.0.0.1' || $_SERVER['REMOTE_ADDR']=='::1');
+     if (!$local )
+     {
+         $result = new mysqli('127.0.0.1:53181', 'azure', '6#vWHD_$', 'sports');
+     }
+     else {
+         //$result = new mysqli('localhost', 'bm_user', 'password', 'bookmarks');
+         $result = new mysqli('localhost', 'root', '', 'sports');
+     }
+ 
+
 
    if (!$result) {
      throw new Exception('Could not connect to database server');
